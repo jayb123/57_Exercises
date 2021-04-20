ce5815941b3470e3f9294556ae6fbf0d29bc5d17
jayb123 <31194660+jayb123@users.noreply.github.com>
2021-04-19 21:36:26 -0500

    Add files via upload
    Day 1 - sayingHello v1.0

---
+++ SayingHello v1.0.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 19 21:28:41 2021
+
+@author: jbinstein
+"""
+
+
+name = input("What is your name?")
+
+print("Hello, " + name + ", nice to meet you!")

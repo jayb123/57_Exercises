7fa9a0a035397cb2cb25a6353f96b3a125a410f5
jayb123 <31194660+jayb123@users.noreply.github.com>
2021-05-06 11:44:52 -0500

    Add files via upload

---
+++ comparingNumbers.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  6 11:11:04 2021
+
+@author: jbinstein
+"""
+
+
+#comparing numbers
+
+first = int(input("first: "))
+second = int(input("second: "))
+third = int(input("third: "))
+
+if first == second or first == third or second == third:
+    print("no two can be the same")
+else:
+    print(max(first,second,third))
+    
+    

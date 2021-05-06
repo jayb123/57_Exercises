7f8f1c08b7e9d3d57802e44343e4863fad71f0e2
jayb123 <31194660+jayb123@users.noreply.github.com>
2021-05-06 11:57:26 -0500

    Update comparingNumbers.py

--- comparingNumbers.py
+++ comparingNumbers.py
@@ -18,4 +18,22 @@ if first == second or first == third or second == third:
 else:
     print(max(first,second,third))
     
-    
+## challenge write algorithm don't use max
+
+'''
+#comparing numbers
+
+first = int(input("first: "))
+second = int(input("second: "))
+third = int(input("third: "))
+
+if first == second or first == third or second == third:
+    print("no two can be the same")
+else:
+   if first > second and first > third:
+       print(first)
+   elif second > third:
+       print(second)
+   else:
+       print(third)
+ '''
